3fe3fec620062e4ee28abe17bb8370d9d854ad81
manoj kumar <Manojgum@gmail.com>
2023-05-22 19:40:18 +0530

    updated

--- server_without_NLP/models/clientModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-let clientSchema= new mongoose.Schema({
-    query:{type:String, required:true},
-    from:String,
-    createdOn:{type:Date, default:Date.now}
-  });
-  const ClientModel = mongoose.model('clientmessages',clientSchema)
-  module.exports =ClientModel
--- server_without_NLP/models/messageModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const messageSchema = new mongoose.Schema({
-
-});
-
-const MessageModel = mongoose.model("messages", messageSchema);
-
-module.exports = MessageModel;
